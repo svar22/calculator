df6b232f4c1533b779c63191a3240463272a8ab2
Svar Ajwani <ajwanisvar@gmail.com>
2024-07-16 22:17:58 -0700

    Same as previous version, however pseudocode has been added at the bottom of the .js file so that logic can be either edited or completely rewritten.

--- calculator.js
+++ calculator.js
@@ -36,20 +36,20 @@ function operate (firstNumber, secondNumber, operator){
 }
 
 
-let smallDisplay = document.querySelector(".small-display");
+let mainDisplay = document.querySelector(".main-display");
 
 //add values from buttons pressed to small display
 numbers = document.querySelectorAll(".number");
 numbers.forEach((currentButton) => {
     currentButton.addEventListener("click", () => {
-        smallDisplay.textContent += currentButton.textContent;
+        mainDisplay.textContent += currentButton.textContent;
         operatorAtFront = false;
     });
 });
 
 decimal = document.querySelector("#decimal");
 decimal.addEventListener("click", () => {
-    smallDisplay.textContent += ".";
+    mainDisplay.textContent += ".";
     operatorAtFront = false;
 })
 
@@ -58,7 +58,7 @@ operators = document.querySelectorAll(".operator");
 operators.forEach((currentButton) => {
     currentButton.addEventListener("click", () => {
         if (operatorAtFront === false){
-            smallDisplay.textContent += ` ${currentButton.textContent} `;
+            mainDisplay.textContent += ` ${currentButton.textContent} `;
             operatorAtFront = true;
         } else {
             return;
@@ -66,7 +66,12 @@ operators.forEach((currentButton) => {
     });
 })
 
-//when equals is pressed, equation within display is operated on.
-// equals = document.querySelector("#equals");
-// equals.addEventListener("click", (evaluate());
-
+// 1. When number is pressed, number is appended to the larger display.
+// 2. When operator is pressed, number in larger display becomes first number, first number and operator are displayed in smaller display above. First number remains in large display until another number (or decimal point) is pressed.
+// 3. Now, 
+//     1. If number pressed, replace first number in larger display with number that was pressed, now larger display begins collecting second number.
+//     2. If operator pressed, replace current operator with operator that was pressed
+//     3. If equals pressed, do nothing.
+// 4. Once second number is inputted,
+//     1. If equals pressed, evaluate expression using first number and operator from small display and second number from large display. Then small display should show “(first number) (operator) (second number) =“ and large display should show the result of the expression. 
+//     2. If other operator pressed, evaluate equation using first number and operator from small display and second number from large display. In this case, small display should show the result of the expression and the NEW operator that was just pressed, and large display will show the result of the expression waiting to be replaced by the next input.
